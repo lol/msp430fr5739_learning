d45b5e1c392824ce264781ae49c716363f9d0ff2
Gagandeep <gaganpanwar@gmail.com>
2015-01-24 20:03:39 +0530

    better range.

--- accelerometer_led_example/main.c
+++ accelerometer_led_example/main.c
@@ -5,44 +5,47 @@
 void main(void) 
 {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+
     AccelInit(); //Setup the accelerometer pins.
-    SetupAccel();	//Setup the ADC and Accel
-    CalibrateADC(); //Find zero points of all 3 axis
+    SetupAccel();	//Setup the ADC and Accel. ADC can only convert one axis at a time.
+    CalibrateADC(); //Find zero points of all 3 axis.
+
     ADC10CTL0 |= ADC10ENC | ADC10SC; //Start the first sample. If this is not done the ADC10 interupt will not trigger.
-    LEDonboardInit(0);
+
+    LEDonboardInit(0); //for using on-board LEDs.
 
     while(1)
 	{  
-          if (ADCResult_X < CalValue_X - 30)        
+          if (ADCResult_X < CalValue_X - 60)        
           {
           LED_off_all();
           LED_on(7);
           }
-          else if((ADCResult_X > (CalValue_X - 30)) && (ADCResult_X < (CalValue_X-20)))
+          else if((ADCResult_X > (CalValue_X - 60)) && (ADCResult_X < (CalValue_X-40)))
           {
             LED_off_all();
             LED_on(6);
           }
-          else if((ADCResult_X > (CalValue_X - 20)) && (ADCResult_X < (CalValue_X-10)))
+          else if((ADCResult_X > (CalValue_X - 40)) && (ADCResult_X < (CalValue_X-20)))
           {
             LED_off_all();
             LED_on(5);
           }
-          else if((ADCResult_X > (CalValue_X - 10)) && (ADCResult_X < (CalValue_X+10)))
+          else if((ADCResult_X > (CalValue_X - 20)) && (ADCResult_X < (CalValue_X+20)))
           {
             LED_off_all();
           }
-          else if((ADCResult_X < (CalValue_X + 20)) && (ADCResult_X > (CalValue_X+10)))
+          else if((ADCResult_X < (CalValue_X + 40)) && (ADCResult_X > (CalValue_X+20)))
           {
             LED_off_all();
             LED_on(4);
           }
-          else if((ADCResult_X < (CalValue_X + 30)) && (ADCResult_X > (CalValue_X+20)))
+          else if((ADCResult_X < (CalValue_X + 60)) && (ADCResult_X > (CalValue_X+40)))
           {
             LED_off_all();
             LED_on(3);
           }
-          else if(ADCResult_X > CalValue_X+30)
+          else if(ADCResult_X > CalValue_X+60)
           {
             LED_off_all();
             LED_on(2);
